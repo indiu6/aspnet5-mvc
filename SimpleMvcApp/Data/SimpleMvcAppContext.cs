5c2bd2b13185facff18c83a98183606c3b833eba
Sean Yang <hewas6@gmail.com>
2021-09-22 11:34:09 -0400

    readd all

---
+++ SimpleMvcApp/Data/SimpleMvcAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimpleMvcApp.Models
+{
+    public class SimpleMvcAppContext : DbContext
+    {
+        public SimpleMvcAppContext (DbContextOptions<SimpleMvcAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Message> Message { get; set; }
+    }
+}
---
+++ SimpleMvcApp/Migrations/20210821023117_..cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleMvcApp.Migrations
+{
+    public partial class _ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Message",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Message", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Message");
+        }
+    }
+}
